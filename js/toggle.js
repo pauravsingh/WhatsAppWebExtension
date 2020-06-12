6ca8efca054a13b9f6f8555ccf4db30f469078f2
Paurav <paurav@opphub.net>
2020-06-12 01:21:15 -0400

    toggle script

---
+++ js/toggle.js
@@ -0,0 +1,5 @@
+if (document.getElementsByTagName('body')[0].classList.contains('dark')) {
+    document.getElementsByTagName('body')[0].classList.remove('dark');
+} else {
+    document.getElementsByTagName('body')[0].classList.add('dark');
+}
